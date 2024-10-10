ed8f556c8ac2ddb8eae87e94de0006fe5b3c4e26
lakb116 <lakb116@naver.com>
2024-10-10 23:42:50 +0900

    add
    드롭 쿨타임 만듦

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,10 @@ public class Player : MonoBehaviour
     GameObject spawnObject;
     GameObject spawned;
     // Start is called before the first frame update
+    
+    private bool drop_enable = true;
+    
+    [SerializeField] private float drop_cooltime = 1f;
     void Start()
     {
         
@@ -21,11 +25,17 @@ public class Player : MonoBehaviour
         float toX = Mathf.Clamp(mousePos.x, -4 ,4);  //마우스좌표
         transform.position = new Vector3(toX, 4, 0); //플레이어 움직임
 
-        if (Input.GetMouseButtonDown(0)) {  //마우스 버튼 누름
-		spawned = Instantiate(spawnObject, new Vector3(toX, 4, 0), Quaternion.identity);  //
+        if (Input.GetMouseButtonDown(0) && drop_enable == true) {  //마우스 버튼 누름
+		spawned = Instantiate(spawnObject, new Vector3(toX, 4, 0), Quaternion.identity);
+        drop_enable = false;
+        StartCoroutine(Cooldown());  //
 	} else if (Input.GetMouseButtonUp(0)) {
 		spawned.GetComponent<Rigidbody2D>().gravityScale = 3;
 	}
     }
 
+    IEnumerator Cooldown(){
+        yield return new WaitForSeconds(drop_cooltime);
+        drop_enable = true;
+    }
 }
