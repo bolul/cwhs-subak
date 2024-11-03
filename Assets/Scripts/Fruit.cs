f590f03014d45736da0a72fb786390f3ac4d0954
lakb116 <lakb116@naver.com>
2024-11-03 18:51:34 +0900

    save
    sdf

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -9,7 +9,7 @@ public class Fruit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        int MyId = gameObject.GetInstanceID();
     }
 
     // Update is called once per frame
@@ -22,9 +22,14 @@ public class Fruit : MonoBehaviour
     {   
         string MyTag = gameObject.tag;
         if (collision.gameObject.CompareTag(MyTag)){
+            if (collision.gameObject.GetInstanceID() < gameObject.GetInstanceID()){
             Debug.Log("충돌함.");
             Destroy(collision.gameObject);
+            }
+            
         }
+
+        
     }
 }
 
